9dbcbb66378e617d772bf7467dad9bebbfcdad24
Anuj Yadav <anujyadav11a@gmail.com>
2026-03-15 11:15:17 +0530

    feat(routes): add tenant authentication to database creation routes

--- src/routes/database.route/database.route.js
+++ src/routes/database.route/database.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import {createDatabase} from "../../controllers/database.controller/database.controller.js"
 import {createCollection} from "../../controllers/database.controller/collection.controller.js";
+import {tenantAuthMiddleware} from "../../middleware/tenantAuth.middleware.js"; 
 
 const databaseRouter=new Router();
-databaseRouter.route("/creatDatebase").post(createDatabase);
-databaseRouter.route("/createCollection").post(createCollection);
+databaseRouter.route("/createDatabase").post(tenantAuthMiddleware, createDatabase);
+databaseRouter.route("/createCollection").post(tenantAuthMiddleware, createCollection);
 
 export default databaseRouter;
