8f9cd3d0d08595998fd5edec2197688c9a1ecde2
Anuj Yadav <anujyadav11a@gmail.com>
2026-03-16 16:16:12 +0530

    feat(routes): update database routes,  mouting the collecton methods on database route fro the hierarachy

--- src/routes/database.route/database.route.js
+++ src/routes/database.route/database.route.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import {createDatabase} from "../../controllers/database.controller/database.controller.js"
-import {createCollection} from "../../controllers/database.controller/collection.controller.js";
-import {tenantAuthMiddleware} from "../../middleware/tenantAuth.middleware.js"; 
+import {deleteDatabase} from "../../controllers/database.controller/database.controller.js"
+import { createCollection,listAllCollections } from "../../controllers/database.controller/collection.controller.js";
+
+
+import { authMiddleware } from "../../middleware/auth.middleware.js";
 
 const databaseRouter=new Router();
-databaseRouter.route("/createDatabase").post(tenantAuthMiddleware, createDatabase);
-databaseRouter.route("/createCollection").post(tenantAuthMiddleware, createCollection);
+
+databaseRouter.route("/deleteDatabase/:database_id").delete(authMiddleware, deleteDatabase);
+databaseRouter.route("/:database_id/createCollection").post(authMiddleware, createCollection);
+databaseRouter.route("/:database_id/listCollections").get(authMiddleware, listAllCollections);
+
+
+
+
 
 export default databaseRouter;
