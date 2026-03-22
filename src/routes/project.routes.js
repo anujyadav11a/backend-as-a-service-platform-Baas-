6f7611b1d554e546186acbc0bd98f6381cb7a84e
Anuj Yadav <anujyadav11a@gmail.com>
2026-03-22 10:49:07 +0530

    mounted database method on project route for the hierarchy

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -5,13 +5,23 @@ import {
     getProjectForSDK
 } from '../controllers/project.controller.js';
 import { authMiddleware } from '../middleware/auth.middleware.js';
+import { createDatabase,listAllDatabases } from '../controllers/database.controller/database.controller.js';
 
 
      
 const projectRouter =new Router();
 
 projectRouter.route("/create").post(authMiddleware,createProject)
-projectRouter.route("sdkdetails").post(getProjectForSDK)
+projectRouter.route("/sdkdetails").post(getProjectForSDK)
+
+
+
+
+
+
+
+projectRouter.route("/:project_id/createdatabase").post(authMiddleware,  createDatabase);
+projectRouter.route("/:project_id/listdatabases").get(authMiddleware, listAllDatabases);
 
 
 
