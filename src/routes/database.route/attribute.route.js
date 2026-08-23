e626072f3b61f1e6b41a64f2596c3239b7321a3e
Anuj Yadav <anujyadav11a@gmail.com>
2026-08-24 00:38:38 +0530

    feat(validation): integrate validation schemas for attribute routes

--- src/routes/database.route/attribute.route.js
+++ src/routes/database.route/attribute.route.js
@@ -6,18 +6,14 @@ import {
     deleteAttribute 
 } from "../../controllers/database.controller/attribute.controller.js";
 import { authMiddleware } from "../../middleware/auth.middleware.js";
+import { validate } from "../../middleware/validate.js";
+import { addColumnSchema, listAttributesSchema, updateAttributeSchema, deleteAttributeSchema } from "../../validation/attribute.js";
 
 const attributeRouter = new Router();
 
-
-attributeRouter.route("/:collection_id/attributes").post(authMiddleware, addColumn);
-
-
-attributeRouter.route("/:collection_id/attributes").get(authMiddleware, listAttributes);
-
-attributeRouter.route("/:collection_id/attributes/:attribute_id").put(authMiddleware, updateAttribute);
-
-
-attributeRouter.route("/:collection_id/attributes/:attribute_id").delete(authMiddleware, deleteAttribute);
+attributeRouter.route("/:collection_id/attributes").post(authMiddleware, validate(addColumnSchema), addColumn);
+attributeRouter.route("/:collection_id/attributes").get(authMiddleware, validate(listAttributesSchema), listAttributes);
+attributeRouter.route("/:collection_id/attributes/:attribute_id").put(authMiddleware, validate(updateAttributeSchema), updateAttribute);
+attributeRouter.route("/:collection_id/attributes/:attribute_id").delete(authMiddleware, validate(deleteAttributeSchema), deleteAttribute);
 
 export default attributeRouter;
