ea8542ab734c9af8498d909e38d7d04bc75aca1e
Anuj Yadav <anujyadav11a@gmail.com>
2026-08-24 00:39:29 +0530

    feat(validation): add validation middleware to project routes

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -11,20 +11,27 @@ import {
 import { authMiddleware } from '../middleware/auth.middleware.js';
 import { createDatabase,listAllDatabases } from '../controllers/database.controller/database.controller.js';
 import { cacheMiddleware } from '../middleware/redisCache.js';
-   
+import { validate } from '../middleware/validate.js';
+import { 
+    createProjectSchema, 
+    updateProjectSchema, 
+    getProjectSchema, 
+    searchProjectsSchema, 
+    generateApiKeySchema, 
+    deleteProjectSchema 
+} from '../validation/project.js';
+import { createDatabaseSchema, listDatabasesSchema } from '../validation/database.js';
+
 const projectRouter =new Router();
 
-projectRouter.route("/create").post(authMiddleware,createProject)
-projectRouter.route("/sdkdetails").post(authMiddleware,cacheMiddleware("sdk-details"), getProjectForSDK)
-projectRouter.route("/list").get(authMiddleware,cacheMiddleware("project-list"), getUserProjects);
-projectRouter.route("/search").get(authMiddleware, searchProjects);
-projectRouter.route("/:project_id").get(authMiddleware, cacheMiddleware("project"), getProject);
-projectRouter.route("/:projectId").put(authMiddleware, updateProject);
-projectRouter.route("/:projectId").delete(authMiddleware, deleteProject);
-projectRouter.route("/:project_id/createdatabase").post(authMiddleware,  createDatabase);
-projectRouter.route("/:project_id/listdatabases").get(authMiddleware, cacheMiddleware("database-list"), listAllDatabases);
-
-
-
+projectRouter.route("/create").post(authMiddleware, validate(createProjectSchema), createProject);
+projectRouter.route("/sdkdetails").post(authMiddleware, cacheMiddleware("sdk-details"), getProjectForSDK);
+projectRouter.route("/list").get(authMiddleware, cacheMiddleware("project-list"), getUserProjects);
+projectRouter.route("/search").get(authMiddleware, validate(searchProjectsSchema), searchProjects);
+projectRouter.route("/:project_id").get(authMiddleware, validate(getProjectSchema), cacheMiddleware("project"), getProject);
+projectRouter.route("/:projectId").put(authMiddleware, validate(updateProjectSchema), updateProject);
+projectRouter.route("/:projectId").delete(authMiddleware, validate(deleteProjectSchema), deleteProject);
+projectRouter.route("/:project_id/createdatabase").post(authMiddleware, validate(createDatabaseSchema), createDatabase);
+projectRouter.route("/:project_id/listdatabases").get(authMiddleware, validate(listDatabasesSchema), cacheMiddleware("database-list"), listAllDatabases);
 
 export default projectRouter;
