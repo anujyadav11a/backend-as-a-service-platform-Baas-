f04bb7b2e240606cd388234a2ca21d8ed19e364a
Anuj Yadav <anujyadav11a@gmail.com>
2026-08-24 13:25:58 +0530

    refactor(validation): remove unused validation index file

--- src/validation/index.js
+++
@@ -1,7 +0,0 @@
-export * from './common.js';
-export * from './auth.js';
-export * from './project.js';
-export * from './database.js';
-export * from './collection.js';
-export * from './attribute.js';
-export * from './document.js';
